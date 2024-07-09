c33a82663eb9b03d6efccd2afbe27babe4efb0d3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-09 15:49:04 +0530

    selected html elements in js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-    
+const resultElement = document.querySelector("#result");
+const pokemonImage = document.querySelector("#pokemon-image");
+const optionsContainer = document.querySelector("#options");
+const pointsElement = document.querySelector("#points-value");
+const totalCount = document.querySelector("#total-count");
+const mainContainer = document.querySelector(".container");
+const loadingContainer = document.querySelector("#loading-container");
