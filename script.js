cdf20031cb51ca7fec01f45c386e9931c5285fc4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-09 16:22:46 +0530

    added a function to fetch data from poke api

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const pointsElement = document.querySelector("#points-value");
 const totalCount = document.querySelector("#total-count");
 const mainContainer = document.querySelector(".container");
 const loadingContainer = document.querySelector("#loading-container");
+
+async function fetchPokemonById() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+  const data = await response.json();
+  return data;
+}
